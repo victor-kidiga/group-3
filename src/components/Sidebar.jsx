0cfd5c55911d9adefe7dc5cdbf57c13e25b7acbd
Yasmin Dika <yasmine.dika@student.moringaschool.com>
2026-05-12 15:06:25 +0300

    added the navbar.jsx and sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>Admin Panel</h3>
+      <p>Manage MotoGrid cars easily.</p>
+    </aside>
+  );
+}
+
+export default Sidebar;
