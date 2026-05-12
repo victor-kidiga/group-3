a606cd0a36d64eacf322760145cb0f373c0fa269
brucemwendwa <brucemwendwa915@email.com>
2026-05-12 15:11:13 +0300

    added code to featuredcar.jsx app.css,home.jsx

--- src/components/FeaturedCar.jsx
+++ src/components/FeaturedCar.jsx
@@ -1,1 +1,15 @@
+function FeaturedCar({ image, name, price, description }) {
+  return (
+    <div className="featured-card">
+      <img src={image} alt={name} />
 
+      <div className="featured-card-content">
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <h4>{price}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedCar;
