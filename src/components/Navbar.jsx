6dba875e1b5242921adaec2ec3577bd44ec0ec8d
Yasmin Dika <yasmine.dika@student.moringaschool.com>
2026-05-13 17:30:21 +0300

    added a logo to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import carLogo from "../assets/hero.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
-        MotoGrid
+        <img src={carLogo} alt="MotoGrid" />
       </Link>
 
       <div className="nav-links">
