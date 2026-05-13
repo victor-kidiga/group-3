4f109103940b452dbfbe297813cc8b5c726ff5ca
ahmadyusuf <yusufahmadnaim@gmail.com>
2026-05-13 13:23:03 +0300

    deleted changes from Car.jsx

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,5 +1,4 @@
-// Cars.jsx 
-//This will be the main component responsible for fetching and displaying the list of cars.it will also manage the state related to the car data and any search filters.
+
 import React, { useState, useEffect } from 'react';
 import CarCard from '../components/CarCard';
 import SearchBar from '../components/SearchBar';
@@ -9,9 +8,7 @@ const Cars = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch car data from the API
-    //In a real application, you would replace the URL with your actual API endpoint. The fetched data is then stored in the state using setCars.
-     //replace 'https://api.example.com/cars' with the actual API endpoint you are using to fetch the car data. The fetched data is expected to be an array of car objects, which will be stored in the state variable 'cars' using the setCars function. If there is an error during the fetch operation, it will be caught and logged to the console.
+    
     
     fetch('https://api.example.com/cars')
       .then(response => response.json())
